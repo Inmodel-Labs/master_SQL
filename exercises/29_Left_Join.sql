a8f8fb062035c9242c28fe219bdaa9e2be058068
Nitish <99980274+NitishKumar-ai@users.noreply.github.com>
2026-04-17 00:34:13 +0530

    Initialize Lesson: 29_Left_Join

---
+++ exercises/29_Left_Join.sql
@@ -0,0 +1,14 @@
+-- ==========================================
+-- PAGE 29: LEFT JOIN
+-- ==========================================
+
+/*
+  PROBLEM: Inclusive User List
+  We want to see all users and their order IDs, even if they have NEVER 
+  placed an order.
+
+  ANALYSIS: 
+  - Use a 'LEFT JOIN' from 'users' to 'orders'.
+*/
+
+-- TODO: Write your query below.
